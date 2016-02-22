369957b7a9651f60efff792a38f0cf43de37488f
Paul Tagliamonte <tag@pault.ag>
2016-02-21 22:58:01 -0500

    update doc

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+
+This module is experimental and incomplete. Please be careful.
+
+The archive module provides a few `paul.tag/go/debian` compatable bindings
+to read and write Debian apt archives.
+
+*/
+package archive
