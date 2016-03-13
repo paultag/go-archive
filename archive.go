595ea073785cbe5a380ab270b7ec7ceb108889af
Paul Tagliamonte <tag@pault.ag>
2016-03-13 18:15:37 -0400

    add in some helpers

--- archive.go
+++ archive.go
@@ -1,10 +1,14 @@
 package archive
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"path"
 
 	"pault.ag/go/debian/control"
+	"pault.ag/go/debian/dependency"
+	"pault.ag/go/debian/transput"
 )
 
 // Archive {{{
@@ -20,6 +24,9 @@ func NewArchive(root string) Archive {
 func (a Archive) Suite(name string) (*Suite, error) {
 	inRelease := path.Join(a.root, "dists", name, "InRelease")
 	suite := Suite{Binaries: map[string]Binaries{}}
+
+	/* Feature flags */
+	suite.features.Hashes = []string{"sha256", "sha512"}
 
 	fd, err := os.Open(inRelease)
 	if err != nil {
@@ -45,6 +52,15 @@ type Suite struct {
 	Codename    string
 
 	Binaries map[string]Binaries
+
+	features struct {
+		Hashes []string
+		/* Compressors ... */
+	}
+}
+
+func NewHashers(suite Suite, target io.Writer) (io.Writer, []*transput.Hasher, error) {
+	return transput.NewHasherWriters(suite.features.Hashes, target)
 }
 
 func (s Suite) Components() []string {
@@ -57,7 +73,9 @@ func (s Suite) Components() []string {
 
 func (s Suite) AddPackageTo(component string, pkg Package) {
 	if _, ok := s.Binaries[component]; !ok {
-		s.Binaries[component] = Binaries{arches: map[string][]Package{}}
+		s.Binaries[component] = Binaries{
+			arches: map[string][]Package{},
+		}
 	}
 	s.Binaries[component].Add(pkg)
 }
@@ -73,6 +91,46 @@ type Binaries struct {
 func (b Binaries) Add(pkg Package) {
 	arch := pkg.Architecture.String()
 	b.arches[arch] = append(b.arches[arch], pkg)
+}
+
+func (b Binaries) Get(arch dependency.Arch) []Package {
+	return b.arches[arch.String()]
+}
+
+func (b Binaries) Arches() []dependency.Arch {
+	ret := []dependency.Arch{}
+
+	for archName, _ := range b.arches {
+		arch, err := dependency.ParseArch(archName)
+		if err != nil {
+			/* XXX: Wat */
+			continue
+		}
+		ret = append(ret, *arch)
+	}
+	return ret
+}
+
+func (b Binaries) Has(arch dependency.Arch) bool {
+	_, ok := b.arches[arch.String()]
+	return ok
+}
+
+func (b Binaries) WriteArchTo(arch dependency.Arch, out io.Writer) error {
+	encoder, err := control.NewEncoder(out)
+	if err != nil {
+		return err
+	}
+	if packages, ok := b.arches[arch.String()]; ok {
+		for _, pkg := range packages {
+			if err := encoder.Encode(pkg); err != nil {
+				return err
+			}
+		}
+	} else {
+		return fmt.Errorf("No such arch: '%s'", arch)
+	}
+	return nil
 }
 
 // }}}
